ab5a89c0348c8bd271015bfdb2a32f7d4cdf586c
Seth Nickell <seth@gnome.org>
2004-05-28 02:52:23 +0000

    * configure.in: * python/Makefile.am:
    	Include the example python apps in the tarball.
    
    	* python/examples/list-system-services.py
    
    	Add a python new example that fetches the list of services
    	from the system bus.

---
+++ python/examples/list-system-services.py
@@ -0,0 +1,24 @@
+"""Lists services on the system bus
+"""
+import dbus
+
+# Get a connection to the SYSTEM bus
+bus = dbus.Bus(dbus.Bus.TYPE_SYSTEM)
+
+# Get the service provided by the dbus-daemon named org.freedesktop.DBus
+dbus_service = bus.get_service('org.freedesktop.DBus')
+    
+# Get a reference to the desktop bus' standard object, denoted
+# by the path /org/freedesktop/DBus. The object /org/freedesktop/DBus
+# implements the 'org.freedesktop.DBus' interface
+dbus_object = dbus_service.get_object('/org/freedesktop/DBus',
+                                      'org.freedesktop.DBus')
+            
+# One of the member functions in the org.freedesktop.DBus interface
+# is ListServices(), which provides a list of all the other services
+# registered on this bus. Call it, and print the list.
+system_service_list = dbus_object.ListServices()
+
+for service in system_service_list:
+    if service[0] != ':':
+        print (service)
