c72e8df76cda925d0bac0e99af94a4d3760e74b1
John (J5) Palmieri <quinticent@phuket.(none)>
2006-07-12 17:34:58 -0400

    * dbus/dbus_glib_bindings.pyx: Prepend includes dbus-glib.h and   dbus-glib-lowlevel withd dbus/ since we are no longer building in tree and   need to get the system's header files

--- dbus/dbus_glib_bindings.pyx
+++ dbus/dbus_glib_bindings.pyx
@@ -1,11 +1,11 @@
 cimport dbus_bindings
 import dbus_bindings
 
-cdef extern from "dbus-glib.h":
+cdef extern from "dbus/dbus-glib.h":
     ctypedef struct GMainContext
     cdef void dbus_g_thread_init ()
 
-cdef extern from "dbus-glib-lowlevel.h":
+cdef extern from "dbus/dbus-glib-lowlevel.h":
     cdef void dbus_connection_setup_with_g_main (dbus_bindings.DBusConnection *connection,
                                                  GMainContext   *context)
 def setup_with_g_main(conn):
