9c70ef78b9368b5fd090dd3aafb3f189cc88616d
John (J5) Palmieri <quinticent@phuket.(none)>
2006-07-12 21:32:57 -0400

    * run-test.sh:   create the .service file before we launch dbus

--- test/run-test.sh
+++ test/run-test.sh
@@ -15,6 +15,14 @@ function die()
 SCRIPTNAME=$0
 MODE=$1
 
+#create service file
+SERVICE_DIR=test/data/valid-service-files/
+SERVICE_FILE=$SERVICE_DIR/test-python.service
+mkdir -p $SERVICE_DIR
+echo "[D-BUS Service]" > $SERVICE_FILE
+echo "Name=org.freedesktop.DBus.TestSuitePythonService" >> $SERVICE_FILE
+echo "Exec=`pwd`/test/test-service.py" >> $SERVICE_FILE
+
 ## so the tests can complain if you fail to use the script to launch them
 export DBUS_TEST_PYTHON_RUN_TEST_SCRIPT=1
 export LD_LIBRARY_PATH="$DBUS_TOP_BUILDDIR/dbus"
@@ -24,15 +32,6 @@ if test -z "$DBUS_TEST_PYTHON_IN_RUN_TEST"; then
   export DBUS_TEST_PYTHON_IN_RUN_TEST
   exec tools/run-with-tmp-session-bus.sh $SCRIPTNAME $MODE
 fi  
-
-#create service file
-SERVICE_DIR=test/data/valid-service-files/
-SERVICE_FILE=$SERVICE_DIR/test-python.service
-mkdir -p $SERVICE_DIR
-echo "[D-BUS Service]" > $SERVICE_FILE
-echo "Name=org.freedesktop.DBus.TestSuitePythonService" >> $SERVICE_FILE
-echo "Exec=`pwd`/test/test-service.py" >> $SERVICE_FILE
-
 
 echo "running test-client.py"
 test/test-client.py || die "test-client.py failed"
