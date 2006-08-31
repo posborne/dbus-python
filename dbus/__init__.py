656bb219b37ac18ad9bc953ac9a2fb2b5fa6234d
Simon McVittie <smcv@celebrin.(none)>
2006-08-31 18:09:23 +0100

    dbus/__init__.py: Add __all__ (listing a sensible set of public API), __docformat__, __version__

--- dbus/__init__.py
+++ dbus/__init__.py
@@ -7,8 +7,21 @@ FIXME
 Which of the imported symbols constitute public API?
 """
 
-from _dbus import *
-from types import *
-
+__all__ = ('Bus', 'SystemBus', 'SessionBus', 'StarterBus',
+           'Interface',
+           'DBusException', 'ConnectionError',
+           'MissingErrorHandlerException', 'MissingReplyHandlerException',
+           'ValidationException', 'IntrospectionParserException',
+           'UnknownMethodException', 'NameExistsException',
+           'ObjectPath', 'ByteArray', 'Signature', 'Byte', 'Boolean',
+           'Int16', 'UInt16', 'Int32', 'UInt32', 'Int64', 'UInt64',
+           'Double', 'String', 'Array', 'Struct', 'Dictionary', 'Variant'
+           )
+__docformat__ = 'restructuredtext'
 version = (0, 70, 0)
+__version__ = '.'.join(map(str, version))
+
+from dbus._dbus import *
+from dbus.types import *
+
 _dbus_main_loop_setup_function = None
