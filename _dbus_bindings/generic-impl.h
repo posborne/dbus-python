7e742ca5b5543ea290175f9351053f9ad6cfd492
Simon McVittie <smcv@celebrin.pseudorandom.co.uk>
2006-11-14 13:58:02 +0000

    Add Glue_immutable_setattro, DEFINE_CHECK, UNUSED attribute. Remove generic repr() functions

--- _dbus_bindings/generic-impl.h
+++ _dbus_bindings/generic-impl.h
@@ -25,86 +25,32 @@
 
 #define DEFERRED_ADDRESS(ADDR) 0
 
+#if defined(__GNUC__)
+#   if __GNUC__ >= 3
+#       define UNUSED __attribute__((__unused__))
+#   else
+#       define UNUSED /*nothing*/
+#   endif
+#else
+#   define UNUSED /*nothing*/
+#endif
+
+#define DEFINE_CHECK(type) \
+static inline int type##_Check (PyObject *o) \
+{ \
+    return (o->ob_type == &type##Type) \
+            || PyObject_IsInstance(o, (PyObject *)&type##Type); \
+}
+
 static PyObject *empty_tuple = NULL;
 
-/* A generic repr() implementation for int subclasses, returning something
- * like 'dbus.Int16(123)'.
- * Equivalent to the following Python:
- * def __repr__(self):
- *     return '%s(%r)' % (self.__class__, int.__repr__(self))
- */
-static PyObject *
-int_subclass_tp_repr (PyObject *self)
+static int
+Glue_immutable_setattro(PyObject *obj UNUSED,
+                        PyObject *name UNUSED,
+                        PyObject *value UNUSED)
 {
-    PyObject *parent_repr = (PyInt_Type.tp_repr)(self);
-    PyObject *my_repr;
-
-    if (!parent_repr) return NULL;
-    my_repr = PyString_FromFormat("%s(%s)", self->ob_type->tp_name,
-                                  PyString_AS_STRING(parent_repr));
-    /* whether my_repr is NULL or not: */
-    Py_DECREF(parent_repr);
-    return my_repr;
-}
-
-/* A generic repr() implementation for long subclasses, returning something
- * like 'dbus.Int64(123L)'.
- * Equivalent to the following Python:
- * def __repr__(self):
- *     return '%s(%r)' % (self.__class__, long.__repr__(self))
- */
-static PyObject *
-long_subclass_tp_repr(PyObject *self)
-{
-    PyObject *parent_repr = (PyLong_Type.tp_repr)(self);
-    PyObject *my_repr;
-
-    if (!parent_repr) return NULL;
-    my_repr = PyString_FromFormat("%s(%s)", self->ob_type->tp_name,
-                                  PyString_AS_STRING(parent_repr));
-    /* whether my_repr is NULL or not: */
-    Py_DECREF(parent_repr);
-    return my_repr;
-}
-
-/* A generic repr() implementation for float subclasses, returning something
- * like 'dbus.Double(123)'.
- * Equivalent to the following Python:
- * def __repr__(self):
- *     return '%s(%r)' % (self.__class__, float.__repr__(self))
- */
-static PyObject *
-float_subclass_tp_repr(PyObject *self)
-{
-    PyObject *parent_repr = (PyFloat_Type.tp_repr)(self);
-    PyObject *my_repr;
-
-    if (!parent_repr) return NULL;
-    my_repr = PyString_FromFormat("%s(%s)", self->ob_type->tp_name,
-                                  PyString_AS_STRING(parent_repr));
-    /* whether my_repr is NULL or not: */
-    Py_DECREF(parent_repr);
-    return my_repr;
-}
-
-/* A generic repr() implementation for str subclasses, returning something
- * like "dbus.ObjectPath('/foo/bar')".
- * Equivalent to the following Python:
- * def __repr__(self):
- *     return '%s(%r)' % (self.__class__, str.__repr__(self))
- */
-static PyObject *
-str_subclass_tp_repr(PyObject *self)
-{
-    PyObject *parent_repr = (PyString_Type.tp_repr)(self);
-    PyObject *my_repr;
-
-    if (!parent_repr) return NULL;
-    my_repr = PyString_FromFormat("%s(%s)", self->ob_type->tp_name,
-                                  PyString_AS_STRING(parent_repr));
-    /* whether my_repr is NULL or not: */
-    Py_DECREF(parent_repr);
-    return my_repr;
+    PyErr_SetString(PyExc_AttributeError, "Object is immutable");
+    return -1;
 }
 
 /* Take the global interpreter lock and decrement the reference count.
