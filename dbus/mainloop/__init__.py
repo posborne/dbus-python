77778920c41ddc3fe99771f4bd0f865e9550868c
Simon McVittie <smcv@celebrin.pseudorandom.co.uk>
2006-11-30 11:08:08 +0000

    dbus/mainloop/__init__.py: Export glib submodule; improve docstrings

--- dbus/mainloop/__init__.py
+++ dbus/mainloop/__init__.py
@@ -1,7 +1,36 @@
-from _dbus_bindings import NativeMainLoop,\
-                           WATCH_READABLE, WATCH_WRITABLE,\
-                           WATCH_HANGUP, WATCH_ERROR,\
-                           NULL_MAIN_LOOP
+import _dbus_bindings
 
-__all__ = ('NativeMainLoop', 'WATCH_READABLE', 'WATCH_WRITABLE',
-           'WATCH_HANGUP', 'WATCH_ERROR', 'NULL_MAIN_LOOP')
+NativeMainLoop = _dbus_bindings.NativeMainLoop
+
+NULL_MAIN_LOOP = _dbus_bindings.NULL_MAIN_LOOP
+"""A null mainloop which doesn't actually do anything.
+
+For advanced users who want to dispatch events by hand. This is almost
+certainly a bad idea - if in doubt, use the GLib main loop found in
+`dbus.mainloop.glib`.
+"""
+
+WATCH_READABLE = _dbus_bindings.WATCH_READABLE
+"""Represents a file descriptor becoming readable.
+Used to implement file descriptor watches."""
+
+WATCH_WRITABLE = _dbus_bindings.WATCH_WRITABLE
+"""Represents a file descriptor becoming readable.
+Used to implement file descriptor watches."""
+
+WATCH_HANGUP = _dbus_bindings.WATCH_HANGUP
+"""Represents a file descriptor reaching end-of-file.
+Used to implement file descriptor watches."""
+
+WATCH_ERROR = _dbus_bindings.WATCH_ERROR
+"""Represents an error condition on a file descriptor.
+Used to implement file descriptor watches."""
+
+__all__ = (
+           # Imported into this module
+           'NativeMainLoop', 'WATCH_READABLE', 'WATCH_WRITABLE',
+           'WATCH_HANGUP', 'WATCH_ERROR', 'NULL_MAIN_LOOP',
+
+           # Submodules
+           'glib'
+           )
