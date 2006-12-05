cb7538d514e968c19c3fea74bd5de89330112ed5
Simon McVittie <smcv@celebrin.pseudorandom.co.uk>
2006-12-05 11:35:51 +0000

    dbus/introspect_parser.py: Add docstring for process_introspection_data()

--- dbus/introspect_parser.py
+++ dbus/introspect_parser.py
@@ -26,6 +26,18 @@ import cStringIO
 import dbus.exceptions as exceptions
 
 def process_introspection_data(data):
+    """Return a dict mapping ``interface.method`` strings to the
+    concatenation of all their 'in' parameters, and mapping
+    ``interface.signal`` strings to the concatenation of all their
+    parameters.
+
+    Example::
+
+    {
+        'com.example.SignalEmitter.OneString': 's',
+        'com.example.MethodImplementor.OneInt32Argument': 'i',
+    }
+    """
     method_map = {}
 
     XMLREADER_START_ELEMENT_NODE_TYPE = 1
