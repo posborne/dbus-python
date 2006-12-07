090091828e1d616171601d68aed00991d93dfd49
Simon McVittie <simon.mcvittie@collabora.co.uk>
2006-12-07 16:53:42 +0000

    examples/list-system-services.py: Print services in a nicer form, once per line. Offer to connect to the session bus instead.

--- dbus/examples/list-system-services.py
+++ dbus/examples/list-system-services.py
@@ -1,22 +1,41 @@
 #!/usr/bin/env python
 
-"""Lists services on the system bus
-"""
+"""Usage: python list-system-services.py [--session|--system]
+List services on the system bus (default) or the session bus."""
+
+import sys
 
 import dbus
 
-# Get a connection to the SYSTEM bus
-bus = dbus.SystemBus()
+def main(argv):
+    factory = dbus.SystemBus
 
-# Get a reference to the desktop bus' standard object, denoted
-# by the path /org/freedesktop/DBus. 
-dbus_object = bus.get_object('org.freedesktop.DBus', '/org/freedesktop/DBus')
+    if len(argv) > 2:
+        sys.exit(__doc__)
+    elif len(argv) == 2:
+        if argv[1] == '--session':
+            factory = dbus.SessionBus
+        elif argv[1] != 'system':
+            sys.exit(__doc__)
 
-# The object /org/freedesktop/DBus
-# implements the 'org.freedesktop.DBus' interface
-dbus_iface = dbus.Interface(dbus_object, 'org.freedesktop.DBus')
+    # Get a connection to the system or session bus as appropriate
+    bus = factory()
 
-# One of the member functions in the org.freedesktop.DBus interface
-# is ListServices(), which provides a list of all the other services
-# registered on this bus. Call it, and print the list.
-print dbus_object.ListNames()
+    # Get a reference to the desktop bus' standard object, denoted
+    # by the path /org/freedesktop/DBus. 
+    dbus_object = bus.get_object('org.freedesktop.DBus', '/org/freedesktop/DBus')
+
+    # The object /org/freedesktop/DBus
+    # implements the 'org.freedesktop.DBus' interface
+    dbus_iface = dbus.Interface(dbus_object, 'org.freedesktop.DBus')
+
+    # One of the member functions in the org.freedesktop.DBus interface
+    # is ListServices(), which provides a list of all the other services
+    # registered on this bus. Call it, and print the list.
+    services = dbus_object.ListNames()
+    services.sort()
+    for service in services:
+        print service
+
+if __name__ == '__main__':
+    main(sys.argv)
