46d28550bbe03ccf9853f072743d879ae7d621ff
Simon McVittie <simon.mcvittie@collabora.co.uk>
2007-05-07 15:53:59 +0100

    dbus/gobject_service.py: Make ExportedGObject __init__ accept GObject properties

--- dbus/gobject_service.py
+++ dbus/gobject_service.py
@@ -39,8 +39,29 @@ class ExportedGObject(gobject.GObject, dbus.service.Object):
     """
     __metaclass__ = ExportedGObjectType
 
-    def __init__(self, conn=None, object_path=None, bus_name=None):
-        gobject.GObject.__init__(self)
+    def __init__(self, conn=None, object_path=None, **kwargs):
+        """Initialize an exported GObject.
+
+        :Parameters:
+            `conn` : dbus.connection.Connection
+                The D-Bus connection or bus
+            `object_path` : str
+                The object path at which to register this object.
+        :Keywords:
+            `bus_name` : dbus.service.BusName
+                A bus name to be held on behalf of this object, or None.
+            `gobject_properties` : dict
+                GObject properties to be set on the constructed object.
+
+                Any unrecognised keyword arguments will also be interpreted
+                as GObject properties.
+            """
+        bus_name = kwargs.pop('bus_name', None)
+        gobject_properties = kwargs.pop('gobject_properties', None)
+
+        if gobject_properties is not None:
+            kwargs.update(gobject_properties)
+        gobject.GObject.__init__(self, **kwargs)
         dbus.service.Object.__init__(self, conn=conn,
                                      object_path=object_path,
                                      bus_name=bus_name)
